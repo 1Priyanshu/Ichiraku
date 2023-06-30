7bacd9715073cf9b1405e00f528326b5dbe25caf
Priyanshu upman <82941802+1Priyanshu@users.noreply.github.com>
2023-06-30 23:30:54 +0530

    Add files via upload

---
+++ lib/data/repository/cart_repo.dart
@@ -0,0 +1,85 @@
+// ignore_for_file: avoid_function_literals_in_foreach_calls
+
+import 'dart:convert';
+
+import 'package:ichiraku/utils/app_constants.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../../models/cart_models.dart';
+
+//encoding-decoding of data is necessary for shared_preferences(local storage),
+// used jsonEncdode and jsonDecode
+//data from server, data to server
+
+class CartRepo {
+  final SharedPreferences sharedPreferences;
+  CartRepo({required this.sharedPreferences});
+
+  List<String> cart = [];
+  List<String> cartHistory = [];
+
+  void addToCartList(List<CartModel> cartList) {
+    // sharedPreferences.remove(AppConstants.CART_LIST);
+    // sharedPreferences.remove(AppConstants.CART_HISTORY_LIST);
+    // return;
+
+    var time = DateTime.now().toString();
+    cart = [];
+    //conveting to strings for sharedPreference only access string
+    cartList.forEach((element) {
+      element.time = time;
+      return cart.add(jsonEncode(element));
+    });
+    sharedPreferences.setStringList(AppConstants.CART_LIST, cart);
+  }
+
+  List<CartModel> getCartList() {
+    List<CartModel> cartList = [];
+    List<String> cart = [];
+    if (sharedPreferences.containsKey(AppConstants.CART_LIST)) {
+      cart = sharedPreferences.getStringList(AppConstants.CART_LIST)!;
+    }
+
+    cart.forEach((element) {
+      cartList.add(CartModel.fromJson(jsonDecode(element)));
+    });
+
+    return cartList;
+  }
+
+  List<CartModel> getCartHistoryList() {
+    if (sharedPreferences.containsKey(AppConstants.CART_HISTORY_LIST)) {
+      cartHistory = [];
+      cartHistory =
+          sharedPreferences.getStringList(AppConstants.CART_HISTORY_LIST)!;
+    }
+    List<CartModel> cartListHistory = [];
+    cartHistory.forEach((element) =>
+        cartListHistory.add(CartModel.fromJson(jsonDecode(element))));
+    return cartListHistory;
+  }
+
+  void addToCartHistoryList() {
+    if (sharedPreferences.containsKey(AppConstants.CART_HISTORY_LIST)) {
+      cartHistory =
+          sharedPreferences.getStringList(AppConstants.CART_HISTORY_LIST)!;
+    }
+    for (int i = 0; i < cart.length; i++) {
+      cartHistory.add(cart[i]);
+    }
+    removeCart();
+    sharedPreferences.setStringList(
+        AppConstants.CART_HISTORY_LIST, cartHistory);
+  }
+
+  void removeCart() {
+    cart = [];
+    sharedPreferences.remove(AppConstants.CART_LIST);
+  }
+
+  void clearCartHistory() {
+    removeCart();
+    cartHistory = [];
+    sharedPreferences.remove(AppConstants.CART_HISTORY_LIST);
+  }
+}
---
+++ lib/data/repository/user_repo.dart
@@ -0,0 +1,12 @@
+import 'package:get/get.dart';
+import 'package:ichiraku/data/api/api_client.dart';
+import 'package:ichiraku/utils/app_constants.dart';
+
+class UserRepo {
+  final ApiClient apiClient;
+  UserRepo({required this.apiClient});
+
+  Future<Response> getUserInfo() async {
+    return await apiClient.getData(AppConstants.USER_INFO_URI);
+  }
+}
